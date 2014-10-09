351b9acd694ffb4cf6a5d03d4b11051a5d5b9afd
David Gonzalez <davidgonzalez@Davids-MacBook.local>
2014-10-09 12:33:13 +0200

    Added Test Class

---
+++ Git Test/TestClasss.h
@@ -0,0 +1,13 @@
+//
+//  TestClasss.h
+//  Git Test
+//
+//  Created by David Gonzalez on 10/9/14.
+//  Copyright (c) 2014 David Gonzalez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClasss : NSObject
+
+@end
---
+++ Git Test/TestClasss.m
@@ -0,0 +1,13 @@
+//
+//  TestClasss.m
+//  Git Test
+//
+//  Created by David Gonzalez on 10/9/14.
+//  Copyright (c) 2014 David Gonzalez. All rights reserved.
+//
+
+#import "TestClasss.h"
+
+@implementation TestClasss
+
+@end
